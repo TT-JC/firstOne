db2786ad6ae6a7753339a1ff1be671af701cef3e
TT-JC <1016399120@qq.com>
2020-08-09 14:08:48 +0800

    35+

--- firstOne/spiders/98rewer.py
+++ firstOne/spiders/98rewer.py
@@ -32,7 +32,7 @@ class JbSpider(scrapy.Spider):
                 jsonLine = json.loads(jsonLinestr)  # 转换数据
                 # 筛选数据（目标记录数据）
                 i = jsonLine['thread']
-                if i <= 250000:
+                if i <= 350000:
                     continue
 
                 item = {}           # 清空字典
